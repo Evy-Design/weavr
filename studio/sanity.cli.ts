9f0657316e0c6917382d3fe31502a78fedb3ea45
Evy <Evy@koolcollective.nl>
2026-09-04 18:32:35 +0200

    Pin Sanity Studio deployment app id

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -11,6 +11,7 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'f7z6miv9toxaopvjqqjz3r98',
   },
   typegen: {
     enabled: true,
